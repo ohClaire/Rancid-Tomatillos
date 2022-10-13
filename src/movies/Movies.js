3f2a1b06254b5b283464fef693b035646b0b2dd6
Hannah.Celemen <hcelemen@icloud.com>
2022-10-13 14:28:53 -0600

    Add changes

--- src/movies/Movies.js
+++ src/movies/Movies.js
@@ -2,30 +2,25 @@ import React from 'react';
 import './Movies.css';
 import Card from '../card/Card';
 
-const Movies = ({ movies }) => {
-  const movieCards = movies.map(movie => {
+const Movies = ({ movies, showMovie }) => {
+  const movieCards = movies.map((movie) => {
     return (
-      
-        <button>
-          <Card 
-            key={movie.id}
-            id={movie.id}
-            title={movie.title}
-            poster={movie.poster_path}
-            backdrop={movie.backdrop_path}
-            rating={movie.average_rating}
-            release={movie.release_date}
-          />
-        </button>
-      
-  )
-})
+      <button>
+        <Card
+          key={movie.id}
+          id={movie.id}
+          title={movie.title}
+          poster={movie.poster_path}
+          backdrop={movie.backdrop_path}
+          rating={movie.average_rating}
+          release={movie.release_date}
+          onClick={() => showMovie(movie.id)}
+        />
+      </button>
+    );
+  });
 
-  return (
-    <div className='movies-container'>
-      {movieCards}
-    </div>
-  )
-}
+  return <div className="movies-container">{movieCards}</div>;
+};
 
 export default Movies;
