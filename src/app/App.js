598f7d9cb02e82040cad4d70e428b60146ad2c76
Hannah.Celemen <hcelemen@icloud.com>
2022-10-13 14:28:08 -0600

    Add event listener for showing single movie

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 import movieData from '../sample-data';
 import Movies from '../movies/Movies';
+import Details from '../card/Details';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
-    this.state = movieData;
+    this.state = {
+      movieData,
+      movie: null,
+    };
   }
+
+  showMovie = (movieID) => {
+    const currentMovie = this.state.movieData.movies.find(
+      (movie) => movie.id === movieID
+    );
+    this.setState({ movie: currentMovie });
+  };
+
+  closeMovie = () => {
+    this.setState({ movie: null });
+  };
 
   render() {
     return (
@@ -16,7 +31,22 @@ class App extends Component {
           <h1 className="heading">Rancid</h1>
           <h2 className="heading">Tomatillos</h2>
         </div>
-        <Movies movies={this.state.movies} />
+        {this.state.movie ? (
+          <Details
+            id={this.state.movie.id}
+            title={this.state.movie.title}
+            poster={this.state.movie.poster_path}
+            backdrop={this.state.movie.backdrop_path}
+            rating={this.state.movie.average_rating}
+            release={this.state.movie.release_date}
+            closeMovie={this.closeMovie}
+          />
+        ) : (
+          <Movies
+            movies={this.state.movieData.movies}
+            showMovie={this.showMovie}
+          />
+        )}
       </main>
     );
   }
