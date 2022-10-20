e283cb95a0a7f989b269cc222d27930353fa4f6d
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-10-20 09:56:36 -0600

    Adjusted Cypress testing for home page

--- cypress/e2e/homePage.cy.js
+++ cypress/e2e/homePage.cy.js
@@ -7,13 +7,13 @@ describe('Rancid Tomatillos Home Page flows', () => {
     cy.get('.h1').contains('Rancid');
     cy.get('.movies-container');
     cy.get('.movies-container').find('.card');
-    cy.get('.movies-container').find('.poster-img');
-    cy.get(':nth-child(1) > .card > .poster-img');
+    cy.get('.movies-container').find('.poster-img').should('have.length', 40);
   });
 
   it('Should be able to select an individual movie card within movies-container', () => {
     cy.get('.card');
     cy.get('.movie-card-btn:nth-child(1)').trigger('mouseover');
     cy.get('.movie-card-btn:nth-child(1)').click();
+    cy.url().should('include', '/694919');
   });
 });
